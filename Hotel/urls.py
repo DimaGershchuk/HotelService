89d671b606aec2de789ddf1a24ed44efc491c444
DimaGershchuk <dimagersuk29@gmail.com>
2025-06-30 11:27:39 +0100

    Fixed main Hotels page routing

--- Hotel/urls.py
+++ Hotel/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import HotelListView, HotelDetailView, RoomDetailView
 
 urlpatterns = [
-    path('hotel-list/', HotelListView.as_view(), name='hotel-list'),
+    path('', HotelListView.as_view(), name='hotel-list'),
     path('hotel-detail/<int:pk>/', HotelDetailView.as_view(), name='hotel-detail'),
     path('room-detail/<int:pk>/', RoomDetailView.as_view(), name='room-detail'),
 ]
